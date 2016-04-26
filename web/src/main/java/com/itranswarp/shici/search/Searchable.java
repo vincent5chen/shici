e1bc3d22301b4bb66f9e3b4474c0d7e5607edfaa
Michael Liao <askxuefeng@gmail.com>
2016-04-26 14:27:12 +0800

    add java doc

--- web/src/main/java/com/itranswarp/shici/search/Searchable.java
+++ web/src/main/java/com/itranswarp/shici/search/Searchable.java
@@ -1,7 +1,17 @@
 package com.itranswarp.shici.search;
 
+/**
+ * Searchable interface for JavaBean.
+ * 
+ * @author michael
+ */
 public interface Searchable {
 
+	/**
+	 * Get document's id.
+	 * 
+	 * @return Id as string.
+	 */
 	String getId();
 
 }
