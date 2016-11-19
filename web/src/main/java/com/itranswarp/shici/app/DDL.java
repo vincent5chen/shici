755048862e15eb76c3924c37472c76ade3f34e8d
Michael Liao <askxuefeng@gmail.com>
2016-11-19 20:01:50 +0800

    update ddl

--- web/src/main/java/com/itranswarp/shici/app/DDL.java
+++ web/src/main/java/com/itranswarp/shici/app/DDL.java
@@ -13,6 +13,7 @@ public class DDL {
 		System.out.println("-- Run initial schema as root --\n");
 		System.out.println("CREATE DATABASE shici;\n");
 		System.out.println("GRANT SELECT, INSERT, UPDATE, DELETE ON shici.* TO shici@localhost identified by \'shici\';\n");
+		System.out.println("USE shici;\n");
 		System.out.println(warpdb.exportSchema());
 	}
 }
